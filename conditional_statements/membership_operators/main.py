539c211340f3db68f5d47bbf098444d4c00032e0
papertyga <>
2026-01-31 05:10:30 +0000

    Membership Operators

---
+++ conditional_statements/membership_operators/main.py
@@ -0,0 +1,10 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+
+# Write your code here
+contains_raw =  "raw" in description
+contains_Imported = "Imported" in description
+print("Contains 'raw':", contains_raw)
+print("Contains 'Imported':", contains_Imported)
