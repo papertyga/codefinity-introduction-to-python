977cd4f3ba1bd88d935ea1f705a43893b5e7d035
papertyga <>
2026-01-31 05:32:29 +0000

    Type Comparisons

---
+++ conditional_statements/type_comparisons/main.py
@@ -0,0 +1,12 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+
+# Write your code here
+price_is_float = type(price) == float
+count_is_int = type(count) == int
+
+
+print("Is price a float?:", price_is_float) 
+print("Is count an integer?:", count_is_int)
