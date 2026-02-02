1898333865fdceb77510f340d7acbe30275232ad
papertyga <>
2026-02-02 16:12:42 +0000

    Tuple Methods

---
+++ other_data_types/tuple_methods/main.py
@@ -0,0 +1,10 @@
+grocery_departments = ("Produce", "Bakery", "Dairy", "Produce", "Seafood", "Bakery", "Produce", "Deli")
+
+# TODO 1: Use the count() method to find how many times 'Produce' appears in grocery_departments
+produce_count = grocery_departments.count("Produce")
+
+# TODO 2: Use the index() method to find the index of the first occurrence of 'Bakery' in grocery_departments
+first_bakery_index = grocery_departments.index("Bakery")
+
+print(produce_count)
+print(first_bakery_index)
