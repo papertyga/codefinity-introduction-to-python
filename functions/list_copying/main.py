865c445c992437b7d745bfd45169d89d7338ae43
papertyga <>
2026-02-06 17:32:27 +0000

    List Copying

---
+++ functions/list_copying/main.py
@@ -0,0 +1,6 @@
+# List of product prices
+product_prices = [1.50, 2.50, 3.00, 0.99, 2.30]
+def apply_discount(prices)
+    
+# Call the function and store the updated prices
+updated_prices = apply_discount(product_prices)
